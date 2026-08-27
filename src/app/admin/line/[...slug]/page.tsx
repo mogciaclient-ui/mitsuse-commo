dd4ae291eddc6e75a14bb11e14ee63cd87d2b6a2
mogciaclient-ui <mogcia.client@gmail.com>
2026-08-27 23:37:42 +0900

    Split broadcast list and composer

--- src/app/admin/line/[...slug]/page.tsx
+++ src/app/admin/line/[...slug]/page.tsx
@@ -2,5 +2,5 @@ import { LineAdminPage } from "@/components/line-admin-pages";
 
 export default async function Page({ params }: { params: Promise<{ slug: string[] }> }) {
   const { slug } = await params;
-  return <LineAdminPage section={slug[0] ?? "users"} />;
+  return <LineAdminPage section={slug.join("/") || "users"} />;
 }
